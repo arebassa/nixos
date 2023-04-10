f8bd3cdb6a46a0b8308f6186a2e418784464981b
bashfulfobot <dustin@bashfulfobot.com>
2023-04-10 12:20:08 -0700

    test nextdns systemd-resolved

--- hosts/x13/configuration.nix
+++ hosts/x13/configuration.nix
@@ -34,16 +34,16 @@
   networking.nameservers =
     [ "45.90.28.0#81aec9.dns.nextdns.io" "45.90.30.0#81aec9.dns.nextdns.io" ];
 
-  services.resolved = {
-    enable = true;
-    dnssec = "true";
-    domains = [ "~." ];
-    fallbackDns =
-      [ "45.90.28.0#81aec9.dns.nextdns.io" "45.90.30.0#81aec9.dns.nextdns.io" ];
-    extraConfig = ''
-      DNSOverTLS=yes
-    '';
-  };
+  #  services.resolved = {
+  #    enable = true;
+  #    dnssec = "true";
+  #    domains = [ "~." ];
+  #    fallbackDns =
+  #      [ "45.90.28.0#81aec9.dns.nextdns.io" "45.90.30.0#81aec9.dns.nextdns.io" ];
+  #    extraConfig = ''
+  #      DNSOverTLS=yes
+  #    '';
+  #  };
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
