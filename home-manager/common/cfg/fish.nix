cd9c44d4457bfcdbe85b8104d1b766c62052bc02
bashfulrobot <dustin@bashfulrobot.com>
2023-04-11 18:18:56 -0700

    refactor: :recycle: remove old fish code Changes to be committed: 	modified:   home-manager/common/cfg/fish.nix

--- home-manager/common/cfg/fish.nix
+++ home-manager/common/cfg/fish.nix
@@ -7,16 +7,6 @@ in {
     shellInit = ''
       set -x NIXOS_OZONE_WL 1
     '';
-    # # fish plugins
-    # plugins = [{
-    #   name = "fish-ssh-agent";
-    #   src = pkgs.fetchFromGitHub {
-    #     owner = "danhper";
-    #     repo = "fish-ssh-agent";
-    #     rev = "v0.4.0";
-    #     sha256 = "1w8bzv1ic0d6fcznr6g2f6plngv9yc3rmq7hhsd4w5ck8zzv6czj";
-    #   };
-    # }];
 
     # fish functions
     functions = {
