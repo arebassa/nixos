cabe55acfe7668df1bffc93106bc8b6292b3a077
Dustin Krysak <dustin@bashfulrobot.com>
2023-04-12 17:33:02 -0700

    refactor: :recycle: starship cfg Changes to be committed: 	modified:   home-manager/common/cfg/fish.nix

--- home-manager/common/cfg/fish.nix
+++ home-manager/common/cfg/fish.nix
@@ -8,12 +8,10 @@ in {
       set -x NIXOS_OZONE_WL 1
     '';
 
-    plugins = [
-      {
-        name = "autopair";
-        inherit (pkgs.fishPlugins.autopair) src;
-      }
-    ];
+    plugins = [{
+      name = "autopair";
+      inherit (pkgs.fishPlugins.autopair) src;
+    }];
 
     # fish functions
     functions = {
@@ -37,13 +35,88 @@ in {
     enable = true;
     enableFishIntegration = true;
     settings = {
-      # https://starship.rs/config/#prompt
-      prompt = "🐚 ";
-      # https://starship.rs/config/#add_newline
-      # Inserts a blank line between shell prompts
       add_newline = true;
-      # https://starship.rs/config/#format
-      format = "$prompt $username $directory $git_branch $git_status $cmd_duration $line_break $jobs $character";
+      character = {
+        success_symbol = "[λ](bold green)";
+        error_symbol = "[λ](bold red)";
+      };
+      format = builtins.concatStringsSep "" [
+        "$username"
+        "$hostname"
+        "$shlvl"
+        "$kubernetes"
+        "$directory"
+        # "$vcsh"
+        "$git_branch"
+        "$git_commit"
+        "$git_state"
+        "$git_status"
+        # "$hg_branch"
+        # "$docker_context"
+        "$package"
+        # "$cmake"
+        # "$dart"
+        # "$deno"
+        # "$dotnet"
+        "$elixir"
+        # "$elm"
+        "$erlang"
+        "$golang"
+        "$helm"
+        "$java"
+        # "$julia"
+        # "$kotlin"
+        # "$nim"
+        "$nodejs"
+        "$ocaml"
+        "$perl"
+        # "$php"
+        "$purescript"
+        "$python"
+        # "$red"
+        "$ruby"
+        "$rust"
+        # "$scala"
+        # "$swift"
+        "$terraform"
+        # "$vagrant"
+        # "$zig"
+        "$nix_shell"
+        # "$conda"
+        "$memory_usage"
+        "$aws"
+        # "$gcloud"
+        # "$openstack"
+        "$env_var"
+        # "$crystal"
+        # "$custom"
+        "$cmd_duration"
+        # "$line_break"
+        # "$lua"
+        "$jobs"
+        # "$battery"
+        "$time"
+        "$line_break" # added
+        "$status"
+        # "$shell"
+        "$character"
+      ];
+      git_branch.symbol = "🌱 ";
+      git_commit.tag_disabled = false;
+      git_status = {
+        ahead = "⇡\${count}";
+        behind = "⇣\${count}";
+        diverged = "⇕⇡\${ahead_count}⇣\${behind_count}";
+        staged = "+$count";
+      };
+      kubernetes.disabled = false;
+      nix_shell = {
+        format = "via [$symbol$state]($style) ";
+        impure_msg = "ι";
+        pure_msg = "﻿ρ";
+        symbol = "❄️";
+      };
+      time.disabled = false;
     };
   };
   # oh-my-posh
