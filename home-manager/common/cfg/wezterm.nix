419b3fcc4d6f1fe129e61437444e9154bc92ac03
bashfulrobot <dustin@bashfulrobot.com>
2023-04-15 09:59:25 -0700

    feat(wezterm.nix): add support for dynamic color scheme based on appearance (light or dark) and use Black Metal (Nile) (base16) for dark appearance and Berkeley Mono Variable for font

--- home-manager/common/cfg/wezterm.nix
+++ home-manager/common/cfg/wezterm.nix
@@ -1,9 +1,24 @@
 { pkgs, ... }:
 
+# https://wezfurlong.org/wezterm/colorschemes/b/index.html#black-metal-nile-base16
 {
   programs.wezterm = {
     enable = true;
     extraConfig = ''
+      function get_appearance()
+        if wezterm.gui then
+          return wezterm.gui.get_appearance()
+        end
+        return 'Dark'
+      end
+
+      function scheme_for_appearance(appearance)
+        if appearance:find 'Dark' then
+          return 'Black Metal (Nile) (base16)'
+        else
+          return 'Black Metal (Nile) (base16)'
+        end
+      end
       return {
 
         font = wezterm.font 'Berkeley Mono Variable',
@@ -14,7 +29,10 @@
         -- color_scheme = "Dracula (Official)",
         tab_bar_at_bottom = true,
         use_fancy_tab_bar = true,
-        window_decorations = "TITLE | RESIZE"
+        window_decorations = "TITLE | RESIZE",
+
+      color_scheme = scheme_for_appearance(get_appearance()),
+
 
       }
 
