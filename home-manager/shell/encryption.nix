d457746b3f41e41f0d4aa00fa731d548c0ac60e7
Dustin Krysak <dustin@bashfulrobot.com>
2023-04-21 13:02:10 -0700

    * feat(encryption.nix): enable fish integration for gpg-agent service and add comments

--- home-manager/shell/encryption.nix
+++ home-manager/shell/encryption.nix
@@ -3,7 +3,12 @@
 {
   programs.gpg = { enable = true; };
   # programs.ssh = { startAgent = true; };
-  services.gpg-agent = { enable = true; };
+  services.gpg-agent = {
+    enable = true;
+    enableFishIntegration = true;
+    # May not be needed. Testing
+    # enableSshSupport = true;
+  };
   services.gnome-keyring = {
     enable = true;
     components = [ "pkcs11" "secrets" "ssh" ];
