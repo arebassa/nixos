bdc2de773130a90657128928f81ab45687ca597e
bashfulfobot <dustin@bashfulfobot.com>
2023-04-21 17:58:11 +0000

    * feat(ssh.nix): add configuration for 'camino' host in ssh config file, change 'github.com' host configuration to use IP address instead of hostname, remove 'IdentityAgent' configuration for all hosts except 'camino'

--- home-manager/shell/ssh.nix
+++ home-manager/shell/ssh.nix
@@ -8,6 +8,10 @@ Host *
   IdentityAgent ~/.1password/agent.sock
   AddKeysToAgent yes
 
+Host camino
+  HostName 64.225.50.102
+  User root
+
 Host github.com
   HostName github.com
   IdentityFile ~/.ssh/id_rsa_temp
