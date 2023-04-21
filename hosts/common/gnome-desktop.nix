d51c717a9e27fc1fd5bab23867758c8efb0b18ce
bashfulfobot <dustin@bashfulrobot.com>
2023-04-21 13:08:20 -0700

    * feat(gnome-desktop.nix): remove media-assersion, no longer supported

--- hosts/common/gnome-desktop.nix
+++ hosts/common/gnome-desktop.nix
@@ -43,32 +43,5 @@
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-
-    # High quality BT calls
-    media-session.config.bluez-monitor.rules = [
-      {
-        # Matches all cards
-        matches = [{ "device.name" = "~bluez_card.*"; }];
-        actions = {
-          "update-props" = {
-            "bluez5.auto-connect" = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
-          };
-        };
-      }
-      {
-        matches = [
-          # Matches all sources
-          {
-            "node.name" = "~bluez_input.*";
-          }
-          # Matches all outputs
-          { "node.name" = "~bluez_output.*"; }
-        ];
-        actions = { "node.pause-on-idle" = false; };
-      }
-    ];
   };
 }
