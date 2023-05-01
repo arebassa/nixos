c11cf0027e66d7567f659bc0419ea878bcd11d6a
Dustin Krysak <dustin@bashfulrobot.com>
2023-04-30 19:39:59 -0700

    feat: :sparkles: add sops-nix to outputs
    - add sops-nix to inputs and import sops module in dustin-krysak nixos configuration
    - add sops-nix homeManagerModule to home-manager configuration in dustin-krysak nixos configuration

--- flake.nix
+++ flake.nix
@@ -18,19 +18,25 @@
 
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, nixos-hardware, ... }: {
+  outputs = inputs@{ self, nixpkgs, home-manager, nixos-hardware, sops-nix, ... }: {
     nixosConfigurations = {
       # dustin-krysak = work laptop hostname
       dustin-krysak = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
+          sops-nix.nixosModules.sops
           ./hosts/x13/configuration.nix
           nixos-hardware.nixosModules.lenovo-thinkpad-x13-yoga
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.dustin = import ./home-manager/home.nix;
+            home-manager.users.dustin = {
+              imports = [
+                ./home-manager/home.nix;
+                sops-nix.homeManagerModule
+              ];
+            };
 
             # Allow unfree packages
             nixpkgs.config.allowUnfree = true;
