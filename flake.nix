d9fc54006ef041dd366dcc68246f73d4ab0ae67c
bashfulfobot <dustin@bashfulrobot.com>
2023-04-30 19:42:31 -0700

    fix: :bug: Incorrect ;

--- flake.nix
+++ flake.nix
@@ -18,54 +18,53 @@
 
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, nixos-hardware, sops-nix, ... }: {
-    nixosConfigurations = {
-      # dustin-krysak = work laptop hostname
-      dustin-krysak = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          sops-nix.nixosModules.sops
-          ./hosts/x13/configuration.nix
-          nixos-hardware.nixosModules.lenovo-thinkpad-x13-yoga
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.dustin = {
-              imports = [
-                ./home-manager/home.nix;
-                sops-nix.homeManagerModule
-              ];
-            };
+  outputs =
+    inputs@{ self, nixpkgs, home-manager, nixos-hardware, sops-nix, ... }: {
+      nixosConfigurations = {
+        # dustin-krysak = work laptop hostname
+        dustin-krysak = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            sops-nix.nixosModules.sops
+            ./hosts/x13/configuration.nix
+            nixos-hardware.nixosModules.lenovo-thinkpad-x13-yoga
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.dustin = {
+                imports =
+                  [ ./home-manager/home.nix sops-nix.homeManagerModule ];
+              };
 
-            # Allow unfree packages
-            nixpkgs.config.allowUnfree = true;
+              # Allow unfree packages
+              nixpkgs.config.allowUnfree = true;
 
-            # Optionally, use home-manager.extraSpecialArgs to pass
-            # arguments to home.nix
-          }
-        ];
-      };
-      # tower = desktop hostname
-      tower = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./hosts/tower/configuration.nix
-          # nixos-hardware.nixosModules.lenovo-thinkpad-x13-yoga
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.dustin = import ./home-manager/home.nix;
+              # Optionally, use home-manager.extraSpecialArgs to pass
+              # arguments to home.nix
+            }
+          ];
+        };
+        # tower = desktop hostname
+        tower = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./hosts/tower/configuration.nix
+            # nixos-hardware.nixosModules.lenovo-thinkpad-x13-yoga
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.dustin = import ./home-manager/home.nix;
 
-            # Allow unfree packages
-            nixpkgs.config.allowUnfree = true;
+              # Allow unfree packages
+              nixpkgs.config.allowUnfree = true;
 
-            # Optionally, use home-manager.extraSpecialArgs to pass
-            # arguments to home.nix
-          }
-        ];
+              # Optionally, use home-manager.extraSpecialArgs to pass
+              # arguments to home.nix
+            }
+          ];
+        };
       };
     };
-  };
 }
