b1716a841d92cbe54680ddd1dd1b51ad636787e5
bashfulrobot <dustin@bashfulrobot.com>
2023-05-02 18:28:27 -0700

    🐛 fix(nix): fix typo in udev rule comment ✨ feat(nix): add udev rules to change permissions of battery threshold attributes.
    Changes to be committed:
    	modified:   hosts/x13/99-thinkpad-thresholds-udev.nix

--- hosts/x13/99-thinkpad-thresholds-udev.nix
+++ hosts/x13/99-thinkpad-thresholds-udev.nix
@@ -1,13 +1,17 @@
 { lib, ... }:
 
 let
-  udevRules = lib.mkAfter ''# Change the permissions of the battery threshold attributes so that they can be modified by ordinary users
-    ACTION=="add|change", KERNEL=="BAT[0-1]", SUBSYSTEM=="power_supply", TEST{0002}!="/sys%p/charge_control_start_threshold", RUN+="/bin/chmod 666 /sys%p/charge_control_start_threshold"
-    ACTION=="add|change", KERNEL=="BAT[0-1]", SUBSYSTEM=="power_supply", TEST{0002}!="/sys%p/charge_control_end_threshold", RUN+="/bin/chmod 666 /sys%p/charge_control_end_threshold"
-    ACTION=="add|change", KERNEL=="BAT[0-1]", SUBSYSTEM=="power_supply", TEST{0002}!="/sys%p/charge_start_threshold", RUN+="/bin/chmod 666 /sys%p/charge_start_threshold"
-    ACTION=="add|change", KERNEL=="BAT[0-1]", SUBSYSTEM=="power_supply", TEST{0002}!="/sys%p/charge_stop_threshold", RUN+="/bin/chmod 666 /sys%p/charge_stop_threshold"
+  udevRules = lib.mkAfter ''
+    # Change the permissions of the battery threshold attributes so that they can be modified by ordinary users
+        ACTION=="add|change", KERNEL=="BAT[0-1]", SUBSYSTEM=="power_supply", TEST{0002}!="/sys%p/charge_control_start_threshold", RUN+="/bin/chmod 666 /sys%p/charge_control_start_threshold"
+        ACTION=="add|change", KERNEL=="BAT[0-1]", SUBSYSTEM=="power_supply", TEST{0002}!="/sys%p/charge_control_end_threshold", RUN+="/bin/chmod 666 /sys%p/charge_control_end_threshold"
+        ACTION=="add|change", KERNEL=="BAT[0-1]", SUBSYSTEM=="power_supply", TEST{0002}!="/sys%p/charge_start_threshold", RUN+="/bin/chmod 666 /sys%p/charge_start_threshold"
+        ACTION=="add|change", KERNEL=="BAT[0-1]", SUBSYSTEM=="power_supply", TEST{0002}!="/sys%p/charge_stop_threshold", RUN+="/bin/chmod 666 /sys%p/charge_stop_threshold"
   '';
-in
-{
+in {
   services.udev.extraRules = udevRules;
+  services.udev.packages = [ extraUdevRules extraHwdbFile ];
+  services.udev.path =
+    [ pkgs.coreutils pkgs.gnused pkgs.gnugrep pkgs.util-linux udev ];
+
 }
