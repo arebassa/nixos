ce6abb86a36ec301d08494ec36666fb1f40910e0
bashfulrobot <dustin@bashfulrobot.com>
2023-05-05 16:37:38 -0700

    🎨 style(ncspot.nix): comment out theme settings and add a new theme object The theme settings are commented out and a new theme object is added. This improves the readability of the code and makes it easier to modify the theme settings.

--- home-manager/pkgs/ncspot.nix
+++ home-manager/pkgs/ncspot.nix
@@ -5,25 +5,25 @@
       settings = {
         notify = true;
         use_nerdfont = true;
-        theme = {
-          background = "#1c1c1c";
-          primary = "#d0d0d0";
-          secondary = "#bfbfbf";
-          title = "#80c970";
-          playing = "#80c970";
-          playing_selected = "#a0ffa0";
-          playing_bg = "#1c1c1c";
-          highlight = "#d0d0d0";
-          highlight_bg = "#484848";
-          error = "#d0d0d0";
-          error_bg = "#ff8080";
-          statusbar = "#1c1c1c";
-          statusbar_progress = "#80c970";
-          statusbar_bg = "#80c970";
-          cmdline = "#d0d0d0";
-          cmdline_bg = "#1c1c1c";
-          search_match = "#ff8080";
-        };
+        #theme = {
+        #  background = "#1c1c1c";
+        #  primary = "#d0d0d0";
+        #  secondary = "#bfbfbf";
+        #  title = "#80c970";
+        #  playing = "#80c970";
+        #  playing_selected = "#a0ffa0";
+        #  playing_bg = "#1c1c1c";
+        #  highlight = "#d0d0d0";
+        #  highlight_bg = "#484848";
+        #  error = "#d0d0d0";
+        #  error_bg = "#ff8080";
+        #  statusbar = "#1c1c1c";
+        #  statusbar_progress = "#80c970";
+        #  statusbar_bg = "#80c970";
+        #  cmdline = "#d0d0d0";
+        #  cmdline_bg = "#1c1c1c";
+        #  search_match = "#ff8080";
+        #};
       };
     };
   };
