79b62f75a7c8823588543384e880cf0e450e3577
bashfulrobot <dustin@bashfulrobot.com>
2023-05-05 16:50:09 -0700

    update ncspot colors.

--- home-manager/pkgs/ncspot.nix
+++ home-manager/pkgs/ncspot.nix
@@ -6,22 +6,22 @@
         notify = true;
         use_nerdfont = true;
         theme = {
-          background = "#2e3436"
-          primary = "#eeeeec"
-          secondary = "#babdb6"
-          title = "#729fcf"
-          playing = "#729fcf"
-          playing_selected = "#ffffff"
-          playing_bg = "#555753"
-          highlight = "#8ae234"
-          highlight_bg = "#2e3436"
-          error = "#fce94f"
-          error_bg = "#a40000"
-          statusbar = "#eeeeec"
-          statusbar_progress = "#729fcf"
-          statusbar_bg = "#555753"
-          cmdline = "#eeeeec"
-          cmdline_bg = "#2e3436"
+          background = "#2e3436";
+          primary = "#eeeeec";
+          secondary = "#babdb6";
+          title = "#729fcf";
+          playing = "#729fcf";
+          playing_selected = "#ffffff";
+          playing_bg = "#555753";
+          highlight = "#8ae234";
+          highlight_bg = "#2e3436";
+          error = "#fce94f";
+          error_bg = "#a40000";
+          statusbar = "#eeeeec";
+          statusbar_progress = "#729fcf";
+          statusbar_bg = "#555753";
+          cmdline = "#eeeeec";
+          cmdline_bg = "#2e3436";
 
         };
       };
