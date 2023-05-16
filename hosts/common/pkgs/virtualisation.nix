a702d89b1504d9c676ac8ab1fec67264a8c56c3b
bashfulrobot <dustin@bashfulrobot.com>
2023-05-16 15:34:41 -0700

    feat: Configure libvirtd for allowed bridges and startup options
    - Enable libvirtd in virtualisation.nix
    - Configure libvirtd with allowed bridges, startup and shutdown options
    
    Changes to be committed:
    	modified:   hosts/common/pkgs/virtualisation.nix

--- hosts/common/pkgs/virtualisation.nix
+++ hosts/common/pkgs/virtualisation.nix
@@ -2,6 +2,12 @@
   virtualisation = {
     docker.enable = true;
     # multipass.enable = true;
+    libvirtd = {
+      enable = true;
+      allowedBridges = true;
+      onBoot = "start";
+      onShutdown = "suspend";
+    };
   };
 
 }
