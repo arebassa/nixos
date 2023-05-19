c8cb2dcc526ca29df5ddca765e37153bea9c60c0
bashfulrobot <dustin@bashfulrobot.com>
2023-05-18 21:43:42 -0700

    feat: :sparkles: install virtualbox, remove libvirtd

--- hosts/common/pkgs/virtualisation.nix
+++ hosts/common/pkgs/virtualisation.nix
@@ -2,11 +2,18 @@
   virtualisation = {
     docker.enable = true;
     # multipass.enable = true;
-    libvirtd = {
-      enable = true;
-      # allowedBridges = "virbr0";
-      onBoot = "start";
-      onShutdown = "suspend";
+    # libvirtd = {
+    #   enable = true;
+    #   # allowedBridges = "virbr0";
+    #   onBoot = "start";
+    #   onShutdown = "suspend";
+    # };
+
+    virtualbox = {
+      host = {
+        enable = true;
+        enableExtensionPack = true;
+      };
     };
   };
 
